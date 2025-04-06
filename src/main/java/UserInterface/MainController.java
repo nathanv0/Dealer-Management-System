c738413b28a36727024f318b8c652207a0f8acf6
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-04-05 19:49:53 -0500

    Delete unnecessary id and methods. Refactor and handle when user select vehicle

--- src/main/java/UserInterface/MainController.java
+++ src/main/java/UserInterface/MainController.java
@@ -1,14 +1,10 @@
 package UserInterface;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import org.example.hellofx.Dealer;
 import org.example.hellofx.ReadXMLFile;
@@ -21,9 +17,6 @@ import java.util.*;
 public class MainController implements Initializable {
 
     @FXML private AnchorPane scenePane;
-    @FXML private MenuItem openFile;
-    @FXML private MenuItem closeApp;
-    @FXML private MenuItem saveFile;
     @FXML private Label outputLabel;
 
     @FXML private Label myLabel;
@@ -36,17 +29,14 @@ public class MainController implements Initializable {
     @FXML private TableColumn<Vehicle, String> modelCol;
     @FXML private TableColumn<Vehicle, String> priceCol;
     @FXML private TableColumn<Vehicle, String> statusCol;
-    @FXML private ListView listView;
 
     @FXML private Button addVehicleButton;
     @FXML private Button deleteVehicleButton;
-
 
     private List<Dealer> dealers;
     private Dealer currentDealer;
     private ReadXMLFile readXMLFile = new ReadXMLFile();
     String filePath = "";
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -59,8 +49,6 @@ public class MainController implements Initializable {
         }
         // Choice box switch dealer: Adding all dealer name to the choiceList
         dealerChoiceBox.getItems().addAll(namesList);
-        // Handling choiceBox action
-        dealerChoiceBox.setOnAction(this::getDealerName);
         // Set default value
         dealerChoiceBox.setConverter(new StringConverter<String>() {
             @Override
@@ -73,38 +61,29 @@ public class MainController implements Initializable {
                 return null;
             }
         });
+        // Handling when user select dealer -> load the table based on selection
+        dealerChoiceBox.setOnAction(this::handleSelectDealer);
 
-
+        // Build the table
         VehicleTable.buildTable(vehicleTable, idCol, typeCol, makeCol, modelCol, priceCol, statusCol);
 
-
-
-
-        // Handle adding vehicle
-        addVehicleButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Dialog addVehicleDialog = new AddVehicleDialog();
-                addVehicleDialog.setTitle("Add Vehicle");
-                Optional<Vehicle> result = addVehicleDialog.showAndWait();
-
-            }
-        });
-
-        Vehicle vehicle1 = new Vehicle("sedan", "123", "20000", "Toyota", "Camry");
-        Vehicle vehicle2 = new Vehicle("sdf", "sdf", "sdf", "sadfsdfd", "Camsdfsdary");
-        List<Vehicle> list = new ArrayList<>();
-        list.add(vehicle1);
-        list.add(vehicle2);
-        //vehicleTable.getItems().addAll(list);
-
-        if (!dealers.isEmpty()) {
-            currentDealer = dealers.get(0);
-
-            vehicleTable.getItems().setAll(currentDealer.getVehicles());
-
-
-        }
+//        // Handle adding vehicle
+//        addVehicleButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                Dialog addVehicleDialog = new AddVehicleDialog();
+//                addVehicleDialog.setTitle("Add Vehicle");
+//                Optional<Vehicle> result = addVehicleDialog.showAndWait();
+//
+//            }
+//        });
+//
+//        Vehicle vehicle1 = new Vehicle("sedan", "123", "20000", "Toyota", "Camry");
+//        Vehicle vehicle2 = new Vehicle("sdf", "sdf", "sdf", "sadfsdfd", "Camsdfsdary");
+//        List<Vehicle> list = new ArrayList<>();
+//        list.add(vehicle1);
+//        list.add(vehicle2);
+//        //vehicleTable.getItems().addAll(list);
 
     }
 
@@ -129,13 +108,20 @@ public class MainController implements Initializable {
         new MenuBarController().exit(scenePane);
     }
 
-
-
-    public void getDealerName(ActionEvent event) {
+    public void handleSelectDealer(ActionEvent event) {
         // Get the value from the user choice
-        String dealerName = dealerChoiceBox.getValue();
-        myLabel.setText(dealerName); // Change the name base on the selected choice
+        String dealerName = dealerChoiceBox.getValue(); // get dealer name
+        myLabel.setText(dealerName); // Change the label based on selected dealer
+
+        // Loop through each dealer
+        for (int i = 0; i < dealers.size(); i++) {
+            if (!dealers.isEmpty()) {
+                currentDealer = dealers.get(i);
+                // If their name matches -> add the data to the table
+                if (currentDealer.getName().equals(dealerName)) {
+                    vehicleTable.getItems().setAll(currentDealer.getVehicles());
+                }
+            }
+        }
     }
-
-
 }
